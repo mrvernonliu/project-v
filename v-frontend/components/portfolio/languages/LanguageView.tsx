a4d19c6e22446f42b51c0c3fe6939bb63630e005
Vernon Liu <mrvernonliu@gmail.com>
2021-06-27 19:51:28 -0700

    made technical skills selectable

--- v-frontend/components/portfolio/languages/LanguageView.tsx
+++ v-frontend/components/portfolio/languages/LanguageView.tsx
@@ -1,9 +1,25 @@
 import * as React from 'react'
 import {Language} from "./types/Language";
 import styles from "../../../styles/LanguageView.module.scss"
+import {useEffect, useState} from "react";
+
+export interface SelectedLanguage {
+    name: string,
+    usageYears: number[]
+}
 
 export default function LanguageView(props: any) {
+    const [selectedLanguages, setSelectedLanguages] = useState<SelectedLanguage[]>([])
     const languages: Language[] = props.languages
+
+    const onSkillClick = (selectedLanguage: SelectedLanguage) => {
+        if (selectedLanguages.filter((language) => language.name === selectedLanguage.name).length !== 0) {
+            setSelectedLanguages(selectedLanguages.filter((language) => language.name !== selectedLanguage.name))
+        } else {
+            setSelectedLanguages(selected => [...selected, selectedLanguage])
+        }
+    }
+
     return (
         <div>
             <h1>Technical Skills</h1>
@@ -11,22 +27,16 @@ export default function LanguageView(props: any) {
             <div>
                 {
                     languages.map((language) => (
-                        <svg className={styles.skillIcons} style={ language.whiteBackground ? {backgroundColor: "white"} : {}} viewBox="0 0 128 128" dangerouslySetInnerHTML={{ __html: language.icon}} key={language.name}>
-                        </svg>
+                        <span
+                            className={styles.skillButton}
+                            key={`${language.name}-technicalskills`}
+                            onClick={() => onSkillClick({name: language.name, usageYears: language.usageYears})}
+                        >
+                            <svg className={styles.skillIcons} style={ language.whiteBackground ? {backgroundColor: "white"} : {}} viewBox="0 0 128 128" dangerouslySetInnerHTML={{ __html: language.icon}} key={language.name}>
+                            </svg>
+                        </span>
                     ))
                 }
-                {/*{*/}
-                {/*    experiences.map((experience) => (*/}
-                {/*        <ExperienceItem*/}
-                {/*            name={experience.name}*/}
-                {/*            title={experience.title}*/}
-                {/*            iconUrl={experience.iconUrl}*/}
-                {/*            startDate={experience.startDate}*/}
-                {/*            endDate={experience.endDate}*/}
-                {/*            techStack={experience.techStack}*/}
-                {/*            key={experience.name}*/}
-                {/*        />*/}
-                {/*    ))}*/}
             </div>
         </div>
     )
